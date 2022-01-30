6e31fe2e32f19bc6063074a85524c789102cb2bc
Vinod_Bubanale <vinodbubanale2000@gmail.com>
2022-01-30 01:01:54 -1200

    Added fn headets

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#Add implementation
 def add(x,y):
      pass
+#Sub implementation
 def sub(x,y):
      pass
+#Mul implementation
 def mul(x,y):
      pass
+#Div implementation
 def div(x,y):
      pass
